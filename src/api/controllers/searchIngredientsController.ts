d10a5d56b87198ee03253a535002076e279cd01c
Yosef Hershberg <berioo2000@gmail.com>
2024-06-26 07:54:28 +0300

    Ingredient.ts

--- src/api/controllers/searchIngredientsController.ts
+++ src/api/controllers/searchIngredientsController.ts
@@ -1,6 +1,6 @@
 import { Response } from 'express';
 import CustomRequest from '../../interfaces/CustomRequest';
-import Ingredient from '../../models/ingredient';
+import Ingredient from '../../models/Ingredient';
 
 const searchIngredientsController = async (req: CustomRequest, res: Response) => {
     const { query } = req.query;
