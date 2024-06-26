e9e1cbd5b744f4ee48c65b3e50f9d08532f7f4a8
Yosef Hershberg <berioo2000@gmail.com>
2024-06-26 08:45:53 +0300

    small

--- src/app.ts
+++ src/app.ts
@@ -16,6 +16,7 @@ const app = express();
 
 app.use(morgan('dev'));
 app.use(helmet());
+
 // app.use(cors({
 //   origin: process.env.NODE_ENV === 'production' ? process.env.CORS_ORIGIN : true,
 // }));
