e1d320480d4fe04ed8cefe59e1f1c0be171f0aa0
Yosef Hershberg <berioo2000@gmail.com>
2024-09-17 11:43:05 +0300

    changed recipe genaration to be faster

--- src/api/services/createRecipe.service.ts
+++ src/api/services/createRecipe.service.ts
@@ -24,6 +24,7 @@ export const createRecipeOperations = {
 
     /**
      * @description This function creates a recipe using OpenAI API and returns a valid JSON
+     * @note We create the title first and the recipe and image simultaneously. This way is faster.
      * @param {string} userId
      * @param {CreateRecipeProps} recipeInput
      * @returns {RecipeWithImage}
@@ -45,12 +46,15 @@ export const createRecipeOperations = {
         kitchenUtils = user.kitchenUtils;
         userIngredients = ingredients.map((ingredient: UserIngredientResponse) => ingredient.name);
 
-        // Create a recipe using OpenAI API
-        const recipe = await createRecipeOperations.createRecipeOpenAI(recipeInput, userIngredients, kitchenUtils);
-
-        // Create an image using OpenAI API
-        const imageUrl = await createRecipeOperations.createImageOpenAI(recipe.title)
-
+        // Create a recipe title using OpenAI API
+        const title = await createRecipeOperations.createRecipeTitle(recipeInput, userIngredients, kitchenUtils);
+        
+        // Create the recipe & the recipe image using OpenAI API
+        const [recipe, imageUrl] = await Promise.all([
+            createRecipeOperations.createRecipeOpenAI(recipeInput, userIngredients, kitchenUtils, title),
+            createRecipeOperations.createImageOpenAI(title, userIngredients)
+        ]);
+                
         // Compress the image
         const base64Image = await compressBase64Image(imageUrl as string, 60); //30 KB
 
@@ -68,7 +72,7 @@ export const createRecipeOperations = {
      * @returns {Recipe} recipe
      */
     createRecipeOpenAI:
-        async (recipeInput: CreateRecipeProps, userIngredients: string[], kitchenUtils: KitchenUtils): Promise<Recipe> => {
+        async (recipeInput: CreateRecipeProps, userIngredients: string[], kitchenUtils: KitchenUtils, title: string): Promise<Recipe> => {
             const { mealSelected, selectedTime, prompt, numOfPeople } = recipeInput;
 
             const maxRetries = 3;
@@ -84,12 +88,15 @@ export const createRecipeOperations = {
                             {
                                 role: "user",
                                 content: `
-                                create a recipe for ${mealSelected} that takes ${selectedTime} minutes
+                                create a ${title} recipe for ${mealSelected} that takes ${selectedTime} minutes
                                 the following ingredients are available: ${userIngredients?.join(', ')}
                                 with the following kitchen utilities: ${kitchenUtils}
-                                the recipe should serve ${numOfPeople} people
-                                add also keep in mind this - ${prompt}
-                                the response that I want you to give me should be a VALID json that looks like this:
+                                the recipe should serve ${numOfPeople} people.
+                                add also keep in mind this - ${prompt}.
+                                Very Important: don't use ingredients that are not available in the list of ingredients provided.
+                                Very Important: don't use kitchen utensils that aren't available in hte data I gave you.
+                                DON'T FORGET:
+                                the response that I want you to give me should be a VALID json without the backticks that looks like this:
                                 {
                                     "title": "Recipe title",
                                     "description": "Recipe description",
@@ -135,12 +142,12 @@ export const createRecipeOperations = {
      * @param {string} recipeTitle
      * @returns {string} valid base64 image
      */
-    createImageOpenAI: async (recipeTitle: string): Promise<string> => {
+    createImageOpenAI: async (recipeTitle: string, userIngredients: string[]): Promise<string> => {
         let imageUrl: string
 
         const response = await openai.images.generate({
             model: "dall-e-3",
-            prompt: `A realistic photo of ${recipeTitle}`,
+            prompt: `A realistic photo of ${recipeTitle} recipe with ingredients: ${userIngredients.join(', ')}`,
             n: 1,
             size: "1024x1024",
             response_format: 'b64_json',
@@ -149,6 +156,58 @@ export const createRecipeOperations = {
         imageUrl = response.data[0].b64_json as string;
 
         return imageUrl;
+    },
+
+    createRecipeTitle: async (recipeInput: CreateRecipeProps, userIngredients: string[], kitchenUtils: KitchenUtils): Promise<string> => {
+        const { mealSelected, selectedTime, prompt } = recipeInput;
+
+        const maxRetries = 3;
+        let attempts = 0;
+        let isValidJson = false;
+
+        let recipeTitle = null;
+
+        while (attempts < maxRetries && !isValidJson) { // Retry until a valid JSON is generated
+            try {
+                const completion = await openai.chat.completions.create({
+                    messages: [
+                        {
+                            role: "user",
+                            content: `
+                            create a title for a recipe recipe for ${mealSelected} that takes ${selectedTime} minutes
+                            the following ingredients are available: ${userIngredients?.join(', ')}
+                            with the following kitchen utilities: ${kitchenUtils}
+                            add also keep in mind this - ${prompt}.
+                            DON'T FORGET:
+                            the response that I want you to give me should be a VALID json without the backticks that looks like this:
+                            {
+                                "title": "Recipe title",
+                            }
+                            NOTE: the json i want you to generate must be a valid json object and without the backticks
+                        `
+                        }
+                    ],
+                    model: "gpt-3.5-turbo",
+                });
+
+                const response = completion.choices[0].message.content as string;
+
+                isValidJson = isValidJSON(response);
+
+                if (isValidJson) {
+                    recipeTitle = JSON.parse(response).title;
+                }
+            } catch (error: any) {
+                logger.error(error.message);
+                attempts++;
+            }
+        }
+
+        if (!isValidJson) {
+            throw new Error('No valid JSON response generated');
+        }
+
+        return recipeTitle;
     }
 
 }
