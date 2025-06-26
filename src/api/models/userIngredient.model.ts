7b79b3bf77d432143ab32ddc6887c5b352b94451
Yosef Hershberg <berioo2000@gmail.com>
2025-06-26 19:16:34 +0300

    wtf13

---
+++ src/api/models/userIngredient.model.ts
@@ -0,0 +1,56 @@
+import mongoose, { Document } from 'mongoose';
+import type { UserIngredient } from '../../types';
+
+export type UserIngredientDocument = UserIngredient & Document & {
+    id: string;
+}
+
+// NOTE: This model does not return json in this format ^^^
+// it is reconstructed to { id, name } in the toJSON and toObject methods
+
+const userIngredientSchema = new mongoose.Schema<UserIngredientDocument>({
+    userId: {
+        type: String,
+        required: true,
+    },
+    ingredientId: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: [String],
+        enum: ['food', 'drink'],
+        required: true,
+    }
+}, {
+    toJSON: {
+        virtuals: true,
+        versionKey: false,
+        transform: (_doc: any, ret: any) => {
+            ret.id = ret.ingredientId;
+            delete ret.ingredientId;
+            delete ret._id;
+            delete ret.userId;
+            return ret;
+        }
+    },
+    toObject: {
+        virtuals: true,
+        versionKey: false,
+        transform: (_doc: any, ret: any) => {
+            ret.id = ret.ingredientId;
+            delete ret.ingredientId;
+            delete ret._id;
+            delete ret.userId;
+            return ret;
+        }
+    }
+});
+
+const UserIngredient = mongoose.model<UserIngredientDocument>('UserIngredient', userIngredientSchema);
+
+export default UserIngredient;
