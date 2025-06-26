69ee531767b12e4de724baee1208c7640af29369
Yosef Hershberg <berioo2000@gmail.com>
2025-06-26 19:13:13 +0300

    wtf11

--- src/api/data-access/userIngredient.da.ts
+++ src/api/data-access/userIngredient.da.ts
@@ -1,3 +1,4 @@
+import UserIngredient from "../models/userIngredients.model"
 import type { DeleteResult } from "mongodb"
 import type { UserIngredientResponse, IngredientType, UserIngredient as UserIngredientType } from "../../types"
 
@@ -46,58 +47,3 @@ export const getUserIngredientsByTypeDB = async (userId: string, type: Ingredien
 export const deleteAllUserIngredientsDB = async (userId: string): Promise<DeleteResult> => {
     return await UserIngredient.deleteMany({ userId }).exec()
 }
-
-import mongoose, { Document } from 'mongoose';
-import type { UserIngredient } from '../../types';
-
-export type UserIngredientDocument = UserIngredient & Document & {
-    id: string;
-}
-
-// NOTE: This model does not return json in this format ^^^
-// it is reconstructed to { id, name } in the toJSON and toObject methods
-
-const userIngredientSchema = new mongoose.Schema<UserIngredientDocument>({
-    userId: {
-        type: String,
-        required: true,
-    },
-    ingredientId: {
-        type: String,
-        required: true,
-    },
-    name: {
-        type: String,
-        required: true,
-    },
-    type: {
-        type: [String],
-        enum: ['food', 'drink'],
-        required: true,
-    }
-}, {
-    toJSON: {
-        virtuals: true,
-        versionKey: false,
-        transform: (_doc: any, ret: any) => {
-            ret.id = ret.ingredientId;
-            delete ret.ingredientId;
-            delete ret._id;
-            delete ret.userId;
-            return ret;
-        }
-    },
-    toObject: {
-        virtuals: true,
-        versionKey: false,
-        transform: (_doc: any, ret: any) => {
-            ret.id = ret.ingredientId;
-            delete ret.ingredientId;
-            delete ret._id;
-            delete ret.userId;
-            return ret;
-        }
-    }
-});
-
-const UserIngredient = mongoose.model<UserIngredientDocument>('UserIngredient', userIngredientSchema);
